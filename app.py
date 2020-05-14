9e01e2c261e79d05715bad77f65c69acbc438bb2
Brendan Kenny <kennbp31@gmail.com>
2020-05-14 07:31:50 -0400

    Email configs updated for Heroku
    Added Boto.s3 and updated requirements

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from boto.s3.connection import S3Connection
 import smtplib
 
 app = Flask(__name__)
@@ -24,13 +25,27 @@ def story():
 @app.route('/contact', methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        server = smtplib.SMTP_SSL("smtp.gmail.com", os.environ["port"])
-        server.login(os.environ["username"], os.environ["pass"])
-        server.sendmail(
-            request.form.get("from_email"),
-            os.environ["to"],
-            "<HTML>" + "Name " + request.form.get("from_name") + "/n" + request.form.get("message_html") + "</HTML>")
-        server.quit()
+
+        # Handle using local variables, will be removed once local testing is complete.
+        if not os.environ["username"]:
+            server = smtplib.SMTP_SSL("smtp.gmail.com", S3Connection(os.environ["port"]))
+            server.login(S3Connection(os.environ["username"]), S3Connection(os.environ["pass"]))
+            server.sendmail(
+                request.form.get("from_email"),
+                S3Connection(os.environ["to"]),
+                "Name: " + request.form.get("from_name") + ", Message: " + request.form.get(
+                    "message_html"))
+            server.quit()
+
+        # Setup for Heroku App Vaiables.
+        else:
+            server = smtplib.SMTP_SSL("smtp.gmail.com", os.environ["port"])
+            server.login(os.environ["username"], os.environ["pass"])
+            server.sendmail(
+                request.form.get("from_email"),
+                os.environ["to"],
+                "Name: " + request.form.get("from_name") + ", Message: " + request.form.get("message_html"))
+            server.quit()
 
     return render_template("contact.html")
 
