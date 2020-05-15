6f90e123ddb39f745ae9e8450ae88199d4e6e8cd
Brendan Kenny <kennbp31@gmail.com>
2020-05-15 08:37:59 -0400

    Modifying Email to use  a class
    Created an email class, will modify and add to, eventually adding unit testing.

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
 import os
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
-<<<<<<< Updated upstream
-from boto.s3.connection import S3Connection
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 
-app = Flask(__name__)
-=======
 from email_class import SendEmail
 
 app = Flask(__name__)
-
 app.secret_key = os.environ["secret_key"]
->>>>>>> Stashed changes
 
 
 @app.route('/')
@@ -34,27 +25,10 @@ def story():
 @app.route('/contact', methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-<<<<<<< Updated upstream
-        msg = MIMEMultipart()
-        msg['From'] = os.environ["from"]
-        msg['To'] = os.environ["to"]
-        msg['Subject'] = 'Portfolio Inquiry'
-        message = ("Name: " + request.form.get("from_name") + " , Email: " + request.form.get("from_email")
-                   + " , Message: " + request.form.get("message_html"))
-        msg.attach(MIMEText(message))
 
-        print(msg.as_string())
-        mailserver = smtplib.SMTP(os.environ["smtp"], os.environ["port"])
-
-        mailserver.login(os.environ["username"], os.environ["pass"])
-
-        mailserver.sendmail(os.environ["from"], os.environ["to"], msg.as_string())
-        mailserver.quit()
-=======
         send_grid = SendEmail(request.form.get("from_name"), request.form.get("from_email"), request.form.get("message_text"))
         send_grid.email()
         flash("Email Sent")
->>>>>>> Stashed changes
         return render_template("contact.html")
 
     return render_template("contact.html")
