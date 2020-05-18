10beab5d1f3efb854124b9f8b1d3548374323551
Brendan Kenny <kennbp31@gmail.com>
2020-05-18 07:08:33 -0400

    Update test_email.py
    added setup values to the test cases to reduce amount of times the email is built and cut down on repeated lines of code.

--- test_email.py
+++ test_email.py
@@ -15,26 +15,24 @@ from email_class import EmailSender, EmailBuilder, Result
 
 class TestEmail(unittest.TestCase):
     """Series of unit tests ensuring that emails are built and sent correctly."""
+    @classmethod
+    def setUpClass(cls):
+        cls.build = EmailBuilder()
+        cls.build.build_email("Test Name", "test@example.com", "Test Message!")
 
     # Ensure that environmental variables are defined and loading correctly
     def test_osvariables(self):
-        build = EmailBuilder()
-        build.build_email("Test Name", "test@example.com", "Test Message!")
-        self.assertEqual('Portfolio Inquiry', build.email_msg["Subject"])
+        self.assertEqual('Portfolio Inquiry', self.build.email_msg["Subject"])
 
-    def test_message_concatentaion(self):
-        build = EmailBuilder()
-        build.build_email("Test Name", "test@example.com", "Test Message!")
+    def test_message_concatenation(self):
         self.assertIn('Name: Test Name , Email: test@example.com , Message: Test Message!'
-                      , str(build.email_msg))
+                      , str(self.build.email_msg))
 
     # check that function returns success when there is not an SMTP Exception
     def test_send_success(self):
         with patch("smtplib.SMTP.sendmail") as mock_smtp, patch("smtplib.SMTP.login") as mock_login:
-            build = EmailBuilder()
-            build.build_email("Test Name", "test@example.com", "Test Message!")
             mailer = EmailSender()
-            result = mailer.send_email(build.email_msg)
+            result = mailer.send_email(self.build.email_msg)
             # Ensure that our smtplib was properly mocked
             self.assertTrue(mock_smtp.called)
             self.assertTrue(mock_login.called)
@@ -43,10 +41,8 @@ class TestEmail(unittest.TestCase):
 
     def test_check_user_pass(self):
         with patch("smtplib.SMTP.login") as mock_login:
-            build = EmailBuilder()
-            build.build_email("Test Name", "test@example.com", "Test Message!")
             mailer = EmailSender()
-            result = mailer.send_email(build.email_msg)
+            result = mailer.send_email(self.build.email_msg)
             # Ensure that our smtplib was properly mocked
             self.assertTrue(mock_login.called)
             # Ensure that the login is called with proper variables
@@ -55,11 +51,9 @@ class TestEmail(unittest.TestCase):
     # check that function returns fail when there is an SMTP Exception
     def test_send_fail(self):
         with patch("smtplib.SMTP.login") as mock_login:
-            build = EmailBuilder()
-            build.build_email("Test Name", "test@example.com", "Test Message!")
             mailer = EmailSender()
             mock_login.SMTPException = True
-            result = mailer.send_email(build.email_msg)
+            result = mailer.send_email(self.build.email_msg)
 
             # Ensure that our smtplib was properly mocked
             self.assertTrue(mock_login.called)
